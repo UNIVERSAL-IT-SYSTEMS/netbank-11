8f59d488492a279fe7840512067f27b9c8daf2a8
liuhp <liuhaipeng_1993@163.com>
2016-11-14 22:50:53 +0800

    testgit

---
+++ src/com/netbank/action/Testgit.java
@@ -0,0 +1,16 @@
+package com.netbank.action;
+
+/**
+ * Created by 1 on 2016-11-14.
+ */
+public class Testgit {
+    public void getVal(){
+        System.out.println("OK!!");
+    }
+    public static void main(String[] args){
+        Testgit t=new Testgit();
+        t.getVal();
+        System.out.println("git success!!!");
+
+    }
+}
